2e87fcf54c271ce4dbc6a43f423f69cab92296f3
Craig Storer <craigstorer@gmail.com>
2020-03-16 18:53:13 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,8 @@ def key_for_min_value(name_hash)
     small_num = nil 
     small_name = nil
     name_hash.each do |name, num|
-      if small_box == nil|| num < num
-         small_box = num
+      if small_num == nil|| num < small_num
+         small_num = num
          small_name = name
       
 end
